99595564f5d4f2133faeabc962125e81b2bbf9ef
paolo-peretti <paolo.peretti@edu.unito.it>
2022-03-07 18:31:45 +0100

    init session

--- extensions.py
+++ extensions.py
@@ -1,10 +1,10 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import timedelta
+
 
 app = Flask(__name__)
-app.permanent_session_lifetime = timedelta(minutes=5)
+
 app.secret_key = 'hello'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:root@localhost/IS"
