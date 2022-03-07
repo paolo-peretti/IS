d987f723f1d1fa8358426610758d288f8c9a5388
paolo-peretti <paolo.peretti@edu.unito.it>
2022-03-07 19:43:52 +0100

    init session

--- extensions.py
+++ extensions.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 
 app.secret_key = 'hello'
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:root@localhost/IS"
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///users.sqlite3"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:root@localhost/IS"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
