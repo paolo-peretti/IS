7e7d03ff012faa8a510948c25e668d2e53c6da29
paolo-peretti <paolo.peretti@edu.unito.it>
2022-04-09 18:08:15 +0200

    reviews

--- main.py
+++ main.py
@@ -750,6 +750,14 @@ def logout():
 
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
 
 
 
